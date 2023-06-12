2b2103eff7f0e5b63e616506c6bb953d8bc892d8
joao.almeida <joao.almeida@sefaz.es.gov.br>
2023-06-12 17:24:18 -0300

    Refazendo o repositório e inserindo o projeto novamente

---
+++ src/main/java/com/mballem/curso/boot/web/controller/ComponenteController.java
@@ -0,0 +1,20 @@
+package com.mballem.curso.boot.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/componentes")
+public class ComponenteController {
+
+	@GetMapping("/cadastrar")
+	public String cadastrar() {
+		return "/componente/cadastro";
+	}
+	
+	@GetMapping("/listar")
+	public String listar() {
+		return "/componente/lista";
+	}
+}
